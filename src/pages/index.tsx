902a67b57db987b79b968bb8610643d4330a44c8
craigrbarnes <craigrbarnes@uchicago.edu>
2024-04-10 14:46:56 -0500

    update brh config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import {
-  LandingPage,
-  LandingPageGetStaticProps as getStaticProps,
+  DiscoveryPage,
+  DiscoveryPageGetServerSideProps as getServerSideProps,
 } from '@gen3/frontend';
+import { registerDiscoveryCustomCellRenderers } from '@/lib/Discovery/CustomCellRenderers';
+import { registerDiscoveryStudyPreviewRenderers } from '@/lib/Discovery/CustomRowRenderers';
 
-export default LandingPage;
-export { getStaticProps };
+registerDiscoveryCustomCellRenderers();
+registerDiscoveryStudyPreviewRenderers();
+
+export default DiscoveryPage;
+
+export { getServerSideProps };
